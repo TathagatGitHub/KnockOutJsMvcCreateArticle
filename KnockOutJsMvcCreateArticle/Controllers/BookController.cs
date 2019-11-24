d20f71e2bb9c5ad569cb941840a642049cbbfa84
Tathagat Dwivedi <tathagat.dwivedi@OceanMediainc.com>
2019-11-23 22:18:09 -0800

    Datatable header, filter

--- KnockOutJsMvcCreateArticle/Controllers/BookController.cs
+++ KnockOutJsMvcCreateArticle/Controllers/BookController.cs
@@ -218,7 +218,7 @@ namespace KnockOutJsMvcCreateArticle.Controllers
                 //var displayedMembers = db.BookDB.ToList().Skip(param.iDisplayStart).Take(param.iDisplayLength);
                 if (param.iDisplayLength!=-1)
                 { var displayedMembers = (from s in db.BookDB
-                                          where s.Isbn == "Isbn12"
+                                          where s.Title == "Title12"
                                           select s).ToList().Skip(param.iDisplayStart).Take(param.iDisplayLength);
                     JsonResult = Json(new
                     {
@@ -256,6 +256,80 @@ namespace KnockOutJsMvcCreateArticle.Controllers
             // Return info.     
             return JsonResult;
 
+        }
+
+        public JsonResult DataProviderAction(string sEcho, int iDisplayStart, int iDisplayLength)
+        {
+            var idFilter = Convert.ToString(Request["sSearch_0"]);
+            var nameFilter = Convert.ToString(Request["sSearch_1"]);
+            var  dateFilter = Convert.ToString(Request["sSearch_2"]);
+            var townFilter = Convert.ToString(Request["sSearch_3"]);
+
+            var fromID = 0;
+            var toID = 0;
+            if (idFilter.Contains('~'))
+            {
+                //Split number range filters with ~
+                fromID = idFilter.Split('~')[0] == "" ? 0 : Convert.ToInt32(idFilter.Split('~')[0]);
+                toID = idFilter.Split('~')[1] == "" ? 0 : Convert.ToInt32(idFilter.Split('~')[1]);
+            }
+            var city = "";
+            if (townFilter.Contains('^'))
+            {
+                //Split number range filters with ~
+                city = townFilter.Substring(1, townFilter.Length-2); // == "" ? 0 : townFilter.Split('~')[0];
+             //   toID = idFilter.Split('~')[1] == "" ? 0 : Convert.ToInt32(townFilter.Split('~')[1]);
+            }
+            DateTime fromDate = DateTime.MinValue;
+            DateTime toDate = DateTime.MaxValue;
+            if (dateFilter.Contains('~'))
+            {
+                //Split date range filters with ~
+                fromDate = dateFilter.Split('~')[0] == "" ?
+                  DateTime.MinValue : Convert.ToDateTime(dateFilter.Split('~')[0]);
+                toDate = dateFilter.Split('~')[1] == "" ?
+                  DateTime.MaxValue : Convert.ToDateTime(dateFilter.Split('~')[1]);
+            }
+
+            var filteredCompanies = db.BookDB
+                                    .Where(c => (fromID == 0 || fromID < c.Id)
+                                                &&
+                                                (toID == 0 || c.Id < toID)
+                                                &&
+                                                (nameFilter == "" || c.Title.ToLower().Contains(nameFilter.ToLower()))
+                                                &&
+                                                (townFilter == "" || c.Isbn == city)
+                                                //&&
+                                                //(fromDate == DateTime.MinValue || fromDate < c.DateCreated)
+                                                //&&
+                                                //(toDate == DateTime.MaxValue || c.DateCreated < toDate)
+                                            );
+
+            //Extract only current page
+            //var displayedCompanies = filteredCompanies.Skip(iDisplayStart).Take(iDisplayLength);
+            var result = (from s in filteredCompanies//db.BookDB
+                          where s.Title == "Title12"
+                                    select s).ToList().Skip(iDisplayStart).Take(iDisplayLength);
+
+            //var result = from c in displayedCompanies
+            //             select new[] {
+            //                        Convert.ToString(c.Id),
+            //                        c.Title,
+            //                        c.AuthorId,
+            //                        c.Title,
+            //                        c.Isbn,
+            //                        c.Synopsis,
+            //                        c.Description
+
+            //                    };
+            return Json(new
+            {
+                sEcho = sEcho,
+                iTotalRecords = db.BookDB.Count(),
+                iTotalDisplayRecords = filteredCompanies.Count(),
+                aaData = result
+            },
+                        JsonRequestBehavior.AllowGet);
         }
     }
 }
